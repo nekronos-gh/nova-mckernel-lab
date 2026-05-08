fb14917c4555e66b50bfb10f209841c58b251d03
Eduardo Rodríguez Sánchez <edubuntu2002@gmail.com>
2026-05-08 12:07:03 +0200

    Create a well strucutred linker file together with enough space for user stacks

--- user/include/heap.h
+++ user/include/heap.h
@@ -2,20 +2,17 @@
 
 #include "types.h"
 
-#define PAGE_BITS 12
-#define PAGE_SIZE (1 << PAGE_BITS)
-// 2 Pages (4KB) per stack
-// 1 Unmapped page for stack overflow
-#define STACK_SIZE ((PAGE_SIZE << 1) + PAGE_SIZE)
-
 class ProcessHeap {
   private:
     mword begin, end;
 
   public:
     static ProcessHeap heap;
+    uint32 user_stack_size;
 
-    ProcessHeap(mword h_begin, mword h_end) : begin(h_begin), end(h_end) {}
+    ProcessHeap(mword h_begin, mword h_end) : begin(h_begin), end(h_end) {
+        user_stack_size = h_begin - h_end;
+    }
 
     void *alloc_stack();
 };
--- user/src/heap.cc
+++ user/src/heap.cc
@@ -1,14 +1,13 @@
 
 #include "heap.h"
 #include "stdio.h"
+#include "symbols.h"
 
-extern char _heap_f, _heap_e;
-
-ProcessHeap ProcessHeap::heap(reinterpret_cast<mword>(&_heap_f),
-                              reinterpret_cast<mword>(&_heap_e));
+ProcessHeap ProcessHeap::heap(reinterpret_cast<mword>(&_heap_start),
+                              reinterpret_cast<mword>(&_heap_end));
 
 void *ProcessHeap::alloc_stack() {
-    if (begin + STACK_SIZE > end) {
+    if (begin + USER_STACK_SIZE > end) {
         printf("Heap full begin 0x%x: start 0x%x:\n", begin, end);
         return nullptr;
     }
@@ -16,6 +15,6 @@ void *ProcessHeap::alloc_stack() {
     void *p = reinterpret_cast<void *>(begin);
 
     // Return end address of the stack allocated
-    begin += STACK_SIZE;
+    begin += USER_STACK_SIZE;
     return p;
 }

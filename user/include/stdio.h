2e34aec4dc0f01595a993b9f591fdd948ba6156f
Eduardo Rodríguez Sánchez <edubuntu2002@gmail.com>
2026-05-08 10:43:39 +0200

    Working templated printf with variadic arguments

--- user/include/stdio.h
+++ user/include/stdio.h
@@ -1,3 +1,9 @@
 #pragma once
+#include "syscall.h"
 
-void printf(const char *);
+template <typename... Args>
+static unsigned printf(char const *fmt, Args... args) {
+    mword _args[] = {reinterpret_cast<mword>(args)..., 0};
+    return syscall(SYS_PRINT, reinterpret_cast<mword>(fmt),
+                   reinterpret_cast<mword>(_args));
+}
--- user/src/stdio.cc
+++
@@ -1,6 +0,0 @@
-#include "stdio.h"
-#include "syscall.h"
-
-void printf(const char *str) {
-    syscall(SYS_PRINT, reinterpret_cast<mword>(str));
-}

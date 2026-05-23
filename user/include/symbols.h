9a9a0d47a94da779bd74eb628eecf1c08ed540f8
Eduardo Rodríguez Sánchez <edubuntu2002@gmail.com>
2026-05-23 21:51:38 +0200

    Decrease user stack size to fit into kernel memory

--- user/include/symbols.h
+++ user/include/symbols.h
@@ -2,5 +2,6 @@ extern char _heap_f[];
 extern char _heap_e[];
 
 #define PAGE_SIZE 0x1000
-#define USER_STACK_PAGES 2048
+// TODO: increase to test real memory allocation
+#define USER_STACK_PAGES 1
 #define USER_STACK_SIZE ((USER_STACK_PAGES + 1) * PAGE_SIZE)
--- user/src/user.cc
+++ user/src/user.cc
@@ -7,6 +7,14 @@
 NORETURN void thread_1() {
     printf("Hello from Thread 1!\n", &thread_1);
     yield();
+    yield();
+    while (1)
+        ;
+}
+
+NORETURN void thread_2() {
+    printf("Hello from Thread 2!\n", &thread_2);
+    yield();
     while (1)
         ;
 }
@@ -15,6 +23,8 @@ EXTERN_C NORETURN void main_func() {
     printf("Hello from user space!\n", &main_func);
 
     clone(&thread_1);
+    yield();
+    clone(&thread_2);
     yield();
 
     printf("Back on track...\n");

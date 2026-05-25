eae177c69fd22ecd079756b98992806c7f4995ae
Eduardo Rodríguez Sánchez <edubuntu2002@gmail.com>
2026-05-25 20:38:16 +0200

    No need for loop init

--- kern/include/scheduler.h
+++ kern/include/scheduler.h
@@ -7,12 +7,8 @@ class Scheduler {
     static constexpr unsigned MAX_ECS = 8; // per-priority queue
 
     struct Queue {
-        Ec *slots[MAX_ECS];
+        Ec *slots[MAX_ECS] = {}; // initialized to nullptr
         unsigned head = 0, tail = 0, count = 0;
-        Queue() : head(0), tail(0), count(0) {
-            for (unsigned i = 0; i < MAX_ECS; i++)
-                slots[i] = 0;
-        }
     };
 
     Queue queues[NUM_PRIORITIES];
